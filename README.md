6a1f04f049362f709f3aa9ed737f9e8ba91207ab
yazgoo <yazgoo@gmail.com>
2024-09-17 21:46:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+<!--
+buffrun<c>: | md2html -f -o README.html && ~/dotfiles/__local/bin/open_or_refresh "$PWD/README.html" 
+-->
+
+have you ever whiched to apply a command within vim easily ?
+
+Buffrun goal is to provide a way to do it, by adding a tag in current file.
+
+# Usage with a file
+
+If you want to run a command on the file you are currently editing, just use:
+
+```
+ buffrun: your_shell_command ${file_path}
+```
+
+`${file_path}` will be replaced with current file_path.
+
+For example, this convert current file to markdown and displays the result in firefox:
+
+```
+ buffrun: grep -Ev '^buffrun' ${file_path} | md2html -o README.html && firefox README.html
+```
+
+Just run `:Buffrun` to apply the operation.
+
+Note that we excluded the line with `^buffrun:` using `grep -v`.
+
+# Usage with a buffer
+
+If instead of using the file, you want to directly apply the transform on a buffer, just start your line with a pipe.
+
+```
+ buffrun: | your_shell_command
+```
+
+If we take previous example:
+
+```
+ buffrun: | md2html -o README.html && firefox README.html
+```
+
+In pipe format, you don't have to remove the `buffrun` keyword.
+
+# Additional options [WIP]
+
+These are not yet implemented.
+
+You can also add options to change the behavior of buffrun, for exmaple, if you want to add a prompt before running the command, you can use:
+
+```
+buffrun<c>: your_shell_command ${file_path}
+```
+
+If you want to automatically run the command when the file is written, you can use:
+
+```
+buffrun<o>: your_shell_command ${file_path}
+```
+
+You can combine both options.
---
+++ lua/buffrun/init.lua
@@ -0,0 +1,62 @@
+local M = {}
+
+function M.run_buffrun_command()
+  local api = vim.api
+  local buf = api.nvim_get_current_buf()
+  local lines = api.nvim_buf_get_lines(buf, 0, -1, false)
+
+  local pattern = "^buffrun(<c>?): "
+  local pattern_pipe = pattern .. "|"
+
+  for _, line in ipairs(lines) do
+    if line:match(pattern) then
+      local confirm = false
+      if line:match("<c>") then
+        confirm = true
+      end
+      if confirm then
+        local answer = vim.fn.input("Do you want to buffrun? [y/N]: ")
+        -- clear message
+        vim.o.statusline = ""
+        vim.cmd('messages clear') 
+        if answer ~= "y" then
+          return
+        end
+      end
+      if line:match(pattern_pipe) then
+        local lines2 = api.nvim_buf_get_lines(buf, 0, -1, false)
+        local escaped_lines = {}
+        for _, line2 in ipairs(lines2) do
+          if not line2:match(pattern_pipe) then
+            line2 = line2:gsub("'", "'\"\'\"'")
+            table.insert(escaped_lines, line2)
+          end
+        end
+
+        local command = line:gsub(pattern_pipe, "")
+        local quoted_lines = table.concat(escaped_lines, '\n')
+        local pipe = io.popen("echo '" ..  quoted_lines .. "' | " .. command, "r")
+        pipe:close()
+      else
+        local file_path = api.nvim_buf_get_name(buf)
+        local command = line:gsub(pattern, ""):gsub("${file_path}", file_path)
+        vim.cmd('! ' .. command)
+      end
+    end
+  end
+end
+
+function M.reload_plugin()
+    package.loaded["buffrun"] = nil
+    local r = require("buffrun")
+    r.setup()
+    return r
+end
+
+function M.setup()
+  local api = vim.api
+  api.nvim_create_user_command('BuffRun', M.run_buffrun_command, {})
+  api.nvim_create_user_command('ReloadBuffrun', M.reload_plugin, {})
+end
+
+return M
